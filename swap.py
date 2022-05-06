c5f9b4e95da33628ad7f56700aa33345006e5537
sandeepnrshetty <104994558+sandeepnrshetty@users.noreply.github.com>
2022-05-06 16:32:00 +0530

    swapping number

---
+++ swap.py
@@ -0,0 +1,35 @@
+#python code swap two number
+#without using another varriable
+
+
+x = 5
+y = 7
+
+print("before swapping: ")
+print("value of x : ", x, " and y : ", y)
+
+#code to swap 'x' and 'y'
+x, y = y, x
+
+print("after swapping: ")
+print("value of x : ", x, " and y : ", y)
+
+a = 10
+b = 20
+print("before swapping")
+print("value of a : ", a, " and b : ", b)
+a, b = b, a
+print("after swapping")
+print("value of a : ", a, " and b : ", b)
+a = 20
+b = 10
+
+a = 10
+b = 20
+print("before swapping")
+print("value of a:",a," and b:",b)
+a, b = b, a
+print("after swapping")
+print("value of a:",a," and b:",b)
+a = 20
+b = 10
